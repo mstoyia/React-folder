76b75f1fec893653b461d2663c39910c612d0cfe
Toyia Joseph <toyia0@gmail.com>
2023-04-07 22:59:47 -0400

    first commit

---
+++ nucampsite23/src/App.js
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import React from 'react';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />}/>
+        <Route path='contact' element={<ContactPage />}/>
+        <Route path='directory' element={<CampsitesDirectoryPage />}/>
+        <Route 
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage />}
+        />
+      </Routes>
+     <Footer />
+    </div>
+  );
+      
+}
+
+export default App;
